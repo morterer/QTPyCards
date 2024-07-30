a246e2dae040435c8d813852d4b0469325d5e6dc
Eric Morter <jacob4242@yahoo.com>
2024-07-30 10:41:06 -0500

    inital commit

---
+++ QTPyCards.ino
@@ -0,0 +1,17 @@
+#include <SdFat.h>                 // SD card & FAT filesystem library
+#include <Button.h>                // Library for easily reading buttons
+#include <Adafruit_GFX.h>          // Core graphics library
+#include <Adafruit_ILI9341.h>      // Hardware-specific library
+#include <Adafruit_ImageReader.h>  // Image-reading functions
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
